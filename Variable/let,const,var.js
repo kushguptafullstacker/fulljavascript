76e91410682033000657b723dce7b092bb59be2b
kush <kush8ar1719a@gmail.com>
2025-02-12 22:33:12 +0530

    firstcode

---
+++ Variable/let,const,var.js
@@ -0,0 +1,27 @@
+// what ia variable and how we use 
+
+// variable- varaible is container which stores data and we can  use this data later and varaible sstores the value 
+
+//3 type of variable
+
+// let , vae , const
+
+let city ="sonipat";
+console.log(city);
+city="haryana"
+console.log(city);
+
+// we can change the let value 
+
+var accountid=1234
+console.log(accountid);
+accountid=321
+console.log(accountid);
+// we can change the value of var but we not use this var today beacuse of block issue and functional scope
+
+const Name="kush"
+console.log(Name);
+Name='yash'
+console.log(Name);
+// we cannot change the value of const 
+
---
+++ datatypes/datatypes.js
@@ -0,0 +1,30 @@
+/// data types in js 
+  //number 
+  // string  
+  // object
+  // boolean
+  //undefined
+  //null
+
+const Name="kush"
+console.log(typeof Name);/// type of string 
+
+const number=(1+1)
+console.log(typeof number); // type of number
+
+const kush={
+   naam:'kush',
+   age:19,
+}
+console.log(typeof kush);/// type of object 
+
+const isloggedin=false
+console.log(typeof isloggedin); /// type of boolean 
+
+
+let myname;
+console.log(typeof myname); // undefiened
+
+const accountid=null
+
+console.log(typeof accountid); /// type of object 
